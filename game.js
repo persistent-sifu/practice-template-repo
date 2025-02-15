5cc3e5fca9fecaf59162efac0a483604eb33debb
Mina2fast <160785184+Mina2fast@users.noreply.github.com>
2025-02-15 09:57:29 +0200

    move player

---
+++ game.js
@@ -0,0 +1,10 @@
+function movePlayer(direction) {
+    return new Promise((resolve, reject) => {
+      console.log(`Moving player ${direction}...`);
+      setTimeout(() => {
+        console.log(`Player moved ${direction}`);
+        resolve();
+      }, 500);
+    });
+  }
+  
--- move.js
+++
@@ -1,9 +0,0 @@
-function movePlayer(direction) {
-  return new Promise((resolve, reject) => {
-    console.log(`Moving player ${direction}...`);
-    setTimeout(() => {
-      console.log(`Player moved ${direction}`);
-      resolve();
-    }, 500);
-  });
-}
