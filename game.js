2f7af0babd9ec090d503a558b59e6efab387f5c2
Joka <silent.nin8@gmail.com>
2025-02-15 10:08:02 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,10 +1,1 @@
-function movePlayer(direction) {
-    return new Promise((resolve, reject) => {
-      console.log(`Moving player ${direction}...`);
-      setTimeout(() => {
-        console.log(`Player moved ${direction}`);
-        resolve();
-      }, 500);
-    });
-  }
-  
+
